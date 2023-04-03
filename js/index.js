0bc078ab4db90fd82fc182ebd6e2dfa935d08fa3
Eric Krukowski <erickruk@amazon.com>
2023-04-02 22:36:19 -0400

    Lab complete

--- js/index.js
+++ js/index.js
@@ -2,31 +2,31 @@ document.addEventListener('DOMContentLoaded', (event)=>{
     const createMonsterDiv = document.getElementById('create-monster')
     const monsterContainerDiv = document.getElementById('monster-container')
     const backButton = document.getElementById('back')
-    backButton.disabled = true //Disables at start, since you start on first page
+    backButton.disabled = true //Disable at start, since you start on first page
     const forwardButton = document.getElementById('forward')
-    const monsterArr = []
+    const monsterLib = []
     let pageArr = [], currentPage =1
     const monsterURL = 'http://localhost:3000/monsters'
+    //Gather monster data from API
     fetch(monsterURL).then(resp => resp.json()).then(data => {
         let count = 0
         for (const newMonster of data) {
             pageArr.push(newMonster) //add monster to current page
             count++
             if (count === 50) { //at 50 monsters, stores each page as an array of monster objects
-                monsterArr.push(pageArr)
+                monsterLib.push(pageArr)
                 count =0
                 pageArr = []
             }
         }
-        if(count>0) monsterArr.push(pageArr) //creates the last page if there are remaining monsters
-        console.log(monsterArr.length)
+        if(count>0) monsterLib.push(pageArr) //creates the last page if there are remaining monsters
     }).then(displayMonsters)
     //display first set of monsters and establish function to display current page
 
     function displayMonsters(){
         while(monsterContainerDiv.hasChildNodes()) (monsterContainerDiv.removeChild(monsterContainerDiv.firstChild)) //removes current page's monsters
         //Monster object contains `${name}` as h2, `Age: ${age}` as h4, `Bio: ${description}` as p
-        monsterArr[currentPage-1].forEach(monster => { //adds each monster on page to container
+        monsterLib[currentPage-1].forEach(monster => { //adds each monster on page to container
             const monsterName = document.createElement('h2')
             monsterName.innerHTML = monster.name
             const monsterAge = document.createElement('h4')
@@ -48,17 +48,43 @@ document.addEventListener('DOMContentLoaded', (event)=>{
         backButton.disabled = false //re-enables back button if moving from page 1
         currentPage++
         displayMonsters()
-        if (currentPage ===monsterArr.length) forwardButton.disabled = true //disables button if on last page
+        if (currentPage ===monsterLib.length) forwardButton.disabled = true //disables button if on last page
     })
-    /* ---- POST EXAMPLE ----
-    POST http://localhost:3000/monsters
-    headers:    {      "Content-Type": "application/json",      Accept: "application/json"    }
-    body:        { name: string, age: number, description: string }
-    */
+    //Creates a form to add monsters
+    const monsterForm = document.createElement('form')
+    const nameInput = document.createElement('input')
+    nameInput.setAttribute('id', 'name')
+    nameInput.setAttribute('placeholder', 'enter name')
+    const ageInput = document.createElement('input')
+    ageInput.setAttribute('id', 'age')
+    ageInput.setAttribute('placeholder', 'enter age')
+    const descInput = document.createElement('input')
+    descInput.setAttribute('id', 'description')
+    descInput.setAttribute('placeholder', 'enter description')
+    const createButton = document.createElement('button')
+    createButton.innerHTML = 'create'
+    createButton.addEventListener('click', (event) => {//Adds new monster to database & displays if necessary
+        event.preventDefault()
+        const newMonsterObj = {
+            name: nameInput.value,
+            age: ageInput.value,
+            description: descInput.value
+        }
+        const postObj = {
+            method: 'post',
+            headers:    {      "Content-Type": "application/json",      Accept: "application/json"    },
+            body:        JSON.stringify(newMonsterObj)
+        }
+        fetch(monsterURL, postObj).then(resp => resp.json()).then(newMonster => {
+            debugger
+            if(monsterLib[monsterLib.length-1].length<50) monsterLib[monsterLib.length-1].push(newMonster) //adds monster to the last page
+            else{//if last page is full, add new page, ensure page is able to be navigated
+                monsterLib.push([newMonster])
+                forwardButton.disabled = false
+             } 
+            displayMonsters()
+        })
+    })
+    monsterForm.append(nameInput, ageInput, descInput, createButton)
+    createMonsterDiv.append(monsterForm)
 })
-
-/*
-notes to self:
-adding a 51st monster to a page, re-enable next page
-adding monster to current (last) page, make sure it displays
-*/
